aa5d427ca7d9750bf523c543b4d401939d8ea421
Cam DeSousa <134101013+bluecrow34@users.noreply.github.com>
2024-02-13 16:03:37 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,80 +1,137 @@
-from flask import Flask, redirect, request, render_template
-from models import db, connect_db, User
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, redirect, request, render_template, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
+from models import User, db, connect_db, Post
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///bio_users'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///bio_user_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'itsasecret'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-
+app.config['SQLALCHEMY_ECHO'] = True 
 toolbar = DebugToolbarExtension(app)
 
+app.app_context().push()
 
 connect_db(app)
+db.create_all
 
-
-
-@app.route('/')
+@app.route("/")
 def home():
-    """Home Page"""
+    """User Options"""
     return render_template('base.html')
 
+@app.route("/new")
+def new_user_app():
+    """New User"""
 
-@app.route('/app')
+    return render_template('new_user.html')
+
+@app.route("/users", methods=["GET"])
 def users_list():
     """User List"""
     users = User.query.order_by(User.last_name, User.first_name).all()
-    return render_template('app_list.html')
+    return render_template('user_list.html', users=users)
 
 
-@app.route('/app/new', methods=[ "GET"])
-def new_user_app():
-    """New Applicant"""
+@app.route("/new", methods=["POST"])
+def add_user(): 
+    
+    new_user =User(
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        image_url=request.form['image_url'] or None)
+    
+    db.session.add(new_user)
+    db.session.commit()
 
-    return render_template('new_app.html')
-
-
-@app.route('/app/new', methods=["POST"])
-def add_user():
-        """New User"""
-        new_user = User(
-            first_name = request.form.get['first_name'],
-            last_name = request.form.get['last_name'],
-            image_url = request.form.get['image_url'] or None)
-
-        db.session.add(new_user)
-        db.session.commit()
-
-        return redirect('base.html')
+    return redirect('/users')
        
-
-@app.route('/app/<int:user-id>/')
+@app.route("/users/<int:user_id>/")
 def user_details(user_id):
      """Users Details List"""
      user = User.query.get_or_404(user_id)
-     return render_template('app_details.html', user=user)
+     return render_template('user_details.html', user=user)
 
-@app.route('/app/<int:user-id>/edit')
-def edit_user(user_id):
-     """Edit Users"""
+@app.route("/users/<int:user_id>/edit")
+def edit_user_view(user_id):
+     """Edit Users View"""
+
      user = User.query.get_or_404(user_id)
      return render_template('edit_users.html', user=user)
 
-@app.route('/app/<int:user-id>/edit', methods=["POST"])
+@app.route("/users/<int:user_id>/edit", methods=["POST"])
 def edit_user(user_id):
-     """Edit Users"""
+     """Edit Users Action"""
      user = User.query.get_or_404(user_id)
-     return render_template('edit_users.html', user=user)
+     user.first_name = request.form['first_name']
+     user.last_name = request.form['last_name']
+     user.image_url = request.form['image_url']
 
-@app.route('/app/<int:user-id>/delete', methods=["POST"])
+     db.session.commit()
+
+     return redirect('/users')
+
+@app.route("/users/<int:user_id>/delete", methods=["POST"])
 def delete_user(user_id):
      """Delete Users"""
-     return redirect('base.html')
+    
+     user = User.query.get_or_404(user_id)
+     db.session.delete(user)
+     db.session.commit()
+     #flash(f"User {user.full_name} deleted!")
+
+     return redirect('/users')
 
 
 
+#### POSTS
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/users/<int:user_id>/new/post")
+def posts_form(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template("new_post.html", user=user)
+
+@app.route("/users/<int:user_id>/new/post", methods=["GET"])
+def new_posts(user_id):
+    user = User.query.get_or_404(user_id)
+    new_post =Post(
+        p_title=request.form['title'],
+        p_content=request.form['content']
+       )
+    
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect('/users/{user.id}')
+
+@app.route("/post/<int:post_id>/")
+def post_view(post_id):
+    
+    post = Post.query.get_or_404(post_id)
+    return render_template('post_details.html', post=post)
+
+
+@app.route("/post/<int:post_id>/edit")
+def edit_post_view(post_id):
+    
+    post = Post.query.get_or_404(post_id)
+    return render_template('edit_post.html', post=post)
+
+@app.route("/post/<int:post_id>/edit", methods=["POST"])
+def edit_post(post_id):
+
+    post = Post.query.get_or_404(post_id)
+    post.p_title=request.form['title'],
+    post.p_content=request.form['content']
+    db.session.commit()
+    
+    return redirect('/users/{user.id}')
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    #flash(f"User {user.full_name} deleted!")
+    return redirect('/users/{user.id}')
--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-
+import datetime
 
 db = SQLAlchemy()
 
-DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/img/1687"
 
 
 class User(db.Model):
@@ -16,15 +17,31 @@ class User(db.Model):
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
 
     posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
-
+    
     @property
     def full_name(self):
 
         return f"{self.first_name} {self.last_name}"
+    
+class Post(db.Model):
+    __tablename__ = "posts"
 
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
+    @property
+    def friendly_date(self):
+        """Return nicely-formatted date."""
 
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
 
+    
 def connect_db(app):
     db.app = app
     db.init_app(app)
